2fcfbabffc076d0d325fb54beb6d18019e451146
Hollyol <HollyolGamu@hotmail.com>
2019-04-17 10:26:44 +0200

    rework MY_hex2str over

--- lib/libMY/libMY_conversions/includes/libMY_conversions.h
+++ lib/libMY/libMY_conversions/includes/libMY_conversions.h
@@ -1,9 +1,12 @@
 #ifndef DEF_LIBMY_CONVERSIONS_H
 #define DEF_LIBMY_CONVERSIONS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /*
  * Converts an integer its hexadecimal representation
  */
-char	*MY_hex2str(int nb);
+char	*MY_hex2str(unsigned long nb, int field_width);
 
 #endif
--- lib/libMY/libMY_conversions/srcs/MY_hex2str.c
+++ lib/libMY/libMY_conversions/srcs/MY_hex2str.c
@@ -1,35 +1,32 @@
 #include "libMY_conversions.h"
 
-char	*MY_hex2char(int nb)
+static int	get_length(unsigned long hex, int field_width)
 {
-	char *str;
-	int len;
-	int i;
-	int bit;
+	int	res;
 
-	bit = 0;
-	len = 0;
-	i = 0;
-	str = NULL;
+	res = (hex == 0);
+	while (hex)
+	{
+		hex /= 16;
+		res++;
+	}
+	res = (res > field_width ? res : field_width);
+	return (res);
+}
 
-	while (nb >> (4 * ++len));
+char		*MY_hex2str(unsigned long hex, int field_width)
+{
+	char	*res;
+	int	len;
 
-	str = (char *)calloc(1, (len + 1) * sizeof(char));
-	if (!str) {
-		fputs("Memory error : Failed to allocate memory for hex string\n", stderr);
+	len = get_length(hex, field_width);
+	if (!(res = malloc((len + 1) * sizeof(char))))
 		return (NULL);
+	res[len] = '\0';
+	while (len)
+	{
+		res[--len] = hex % 16 + ((hex % 16 < 10) ? '0' : 'a' - 10);
+		hex = (hex - (hex % 16)) / 16;
 	}
-
-	while (i < len) {
-		bit = (nb >> (4 * (len - (i + 1)))) - ((nb >> (4 * (len - i))) << 4);
-		if (bit < 10)
-			str[i] = '0' + bit;
-		else
-			str[i] = 'A' + bit - 10;
-		i++;
-	}
-
-	str[len] = '\0';
-
-	return (str);
+	return (res);
 }
